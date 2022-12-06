021bcdc2481e524d19a5b930ef639e464aa17652
jobsons10 <119431232+jobsons10@users.noreply.github.com>
2022-12-06 12:43:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const frases = ["O que é que tem nome mas ninguém acredita?", "O que é perfeito mas não deveria existir?", "O que é que somos mas não podemos tocar?"];
+const randomPhrase = frases[Math.floor(Math.random() * frases.length)];
+const response = ['o impossivel', 'o impossível']
+
+document.getElementById("input").placeholder=randomPhrase;
+
+const isCorrect = function() {
+    const value = (document.getElementById("input").value).toLowerCase();
+    if (value == response[0] || value == response[1]){
+        document.getElementById("segunda-dobra").classList.remove('hide-me');
+        window.scrollTo(0, document.body.scrollHeight)
+    } else{
+        document.getElementById("response").classList.add('wrong');
+        document.getElementById("input").value = "";
+
+    }
+}
+
+const wrongReset = function() {
+    document.getElementById("response").classList.remove('wrong');
+}
